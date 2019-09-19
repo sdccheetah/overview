bc1517c155087b03381c7ecc53e6abcf4958f396
Kellyanne Revenaugh <kellyrevenaugh@gmail.com>
2019-09-19 12:18:07 -0400

    set up boiler plates for mongo db

---
+++ database/Schema.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+let products_list = new Schema({
+  id: Number,
+  name: String,
+  slogan: String,
+  description: String,
+  category: String,
+  default_price: Number
+});
+
+let features = new Schema({
+  feature: String,
+  value: String
+});
+
+let styles = new Schema({
+  style_id: Number,
+  name: String,
+  original_price: String,
+  sale_price: String,
+  default: { type: Number, default: 0 }
+});
+
+let photos = new Schema({
+  thumbnail_url: String,
+  url: String
+});
+
+let related = new Schema({
+  product_id: Number,
+  related_products: { default: [] }
+});
+
+let skus = new Schema({
+  XS: Number,
+  S: Number,
+  M: Number,
+  L: Number,
+  XL: Number
+});
